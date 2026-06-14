5655368d77b3b091395b18927b3442518e01047c
kuma <kuma@kuma.homes>
2026-06-15 05:03:31 +0900

    feat: add database transaction API

--- crates/lilium-database/src/lib.rs
+++ crates/lilium-database/src/lib.rs
@@ -2,6 +2,8 @@ pub mod database;
 pub mod entities;
 pub mod pool;
 pub mod queries;
+pub mod transaction;
 
-pub use database::{Database, DatabaseConfig};
+pub use database::{Database, DatabaseConfig, RawDbConnection};
 pub use pool::{DbPool, DbSession, DbSessionContext};
+pub use transaction::TransactionFuture;
